81dcba26c38d4a622f1089c85bd837c32f4dbfbf
Terry Pan <pttdev123@gmail.com>
2024-10-19 08:50:15 +1000

    feat: add embedding index

---
+++ drizzle/migrations/0006_add-embedding-index.sql
@@ -0,0 +1,1 @@
+CREATE INDEX IF NOT EXISTS "embeddingIndex" ON "glossaries" USING hnsw ("embedding" vector_cosine_ops);
--- drizzle/tables/glossary.ts
+++ drizzle/tables/glossary.ts
@@ -1,24 +1,30 @@
-import { pgTable, text, uuid, vector } from 'drizzle-orm/pg-core';
+import { index, pgTable, text, uuid, vector } from 'drizzle-orm/pg-core';
 import { auditAtFields, auditByFields } from '../audit';
 
-export const glossariesTable = pgTable('glossaries', {
-  id: uuid('id').primaryKey().defaultRandom(),
-  origin: text('origin').notNull(),
-  target: text('target').notNull(),
-  originLang: text('origin_lang'),
-  targetLang: text('target_lang'),
-  originSutraText: text('origin_sutra_text'),
-  targetSutraText: text('target_sutra_text'),
-  sutraName: text('sutra_name'),
-  volume: text('volume'),
-  cbetaFrequency: text('cbeta_frequency'),
-  glossaryAuthor: text('glossary_author'),
-  translationDate: text('translation_date'),
-  discussion: text('discussion'),
-  embedding: vector('embedding', { dimensions: 1536 }),
-  ...auditAtFields,
-  ...auditByFields,
-});
+export const glossariesTable = pgTable(
+  'glossaries',
+  {
+    id: uuid('id').primaryKey().defaultRandom(),
+    origin: text('origin').notNull(),
+    target: text('target').notNull(),
+    originLang: text('origin_lang'),
+    targetLang: text('target_lang'),
+    originSutraText: text('origin_sutra_text'),
+    targetSutraText: text('target_sutra_text'),
+    sutraName: text('sutra_name'),
+    volume: text('volume'),
+    cbetaFrequency: text('cbeta_frequency'),
+    glossaryAuthor: text('glossary_author'),
+    translationDate: text('translation_date'),
+    discussion: text('discussion'),
+    embedding: vector('embedding', { dimensions: 1536 }),
+    ...auditAtFields,
+    ...auditByFields,
+  },
+  (table) => ({
+    embeddingIndex: index('embeddingIndex').using('hnsw', table.embedding.op('vector_cosine_ops')),
+  }),
+);
 
 export type ReadGlossary = typeof glossariesTable.$inferSelect;
 export type CreateGlossary = typeof glossariesTable.$inferInsert;
