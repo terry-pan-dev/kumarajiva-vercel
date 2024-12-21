2c24da80ae335138a66c6fcf1fe1e40fae0c1b2a
Terry Pan <pttdev123@gmail.com>
2024-12-21 22:24:55 +1000

    Preview (#25)
    * feat: fix minor bug
    
    * feat: add edit glossary
    
    * feat: add edit glossary
    
    * feat: add sentry tracking
    
    * fix: small fix regarding working space
    
    * feat: update mome for serverless function
    
    * feat: update mome for serverless function
    
    * feat: update abort controller
    
    * feat: update scrollIssue in safari
    
    * feat: update scrollIssue in safari
    
    * feat: add scroable to glossary
    
    * feat: add scroable to glossary
    
    * feat: add download button
    
    * feat: run parallel fetch
    
    * feat: use action instead of loader

--- app/routes/_app.translation.tsx
+++ app/routes/_app.translation.tsx
@@ -1,5 +1,6 @@
-import { Outlet, useLoaderData, useParams, type MetaFunction } from '@remix-run/react';
-import { json, redirect, type LoaderFunctionArgs } from '@vercel/remix';
+import { Outlet, useLoaderData, useParams, type MetaFunction, useFetcher } from '@remix-run/react';
+import { type ActionFunctionArgs, json, redirect, type LoaderFunctionArgs } from '@vercel/remix';
+import { useEffect, useState } from 'react';
 
 import { type ReadRollWithSutra } from '../../drizzle/tables';
 import { assertAuthUser } from '../auth.server';
@@ -22,31 +23,67 @@ export const loader = async ({ request, params }: LoaderFunctionArgs) => {
     return redirect('/login');
   }
   const allUsers = await readUsers();
-  let paragraphs: IParagraph[] = [];
-  let rollInfo: ReadRollWithSutra | undefined = undefined;
-  if (params.rollId) {
-    paragraphs = await readParagraphsByRollId({ rollId: params.rollId, user });
-    rollInfo = await readRollById(params.rollId);
-  }
   return json({
     user,
     users: allUsers.map((user) => ({ id: user.id, username: user.username, email: user.email })),
+  });
+};
+
+export const action = async ({ request, params }: ActionFunctionArgs) => {
+  const user = await assertAuthUser(request);
+  if (!user) {
+    return redirect('/login');
+  }
+  const formData = await request.formData();
+  const rollId = formData.get('rollId');
+  let paragraphs: IParagraph[] = [];
+  let rollInfo: ReadRollWithSutra | undefined = undefined;
+  if (rollId) {
+    [paragraphs, rollInfo] = await Promise.all([
+      readParagraphsByRollId({ rollId: rollId as string, user }),
+      readRollById(rollId as string),
+    ]);
+  }
+  console.log('downloading for rollId', rollId);
+  return json({
     paragraphs,
     rollInfo,
   });
 };
 
 export default function TranslationLayout() {
-  const { user, users, paragraphs, rollInfo } = useLoaderData<typeof loader>();
+  const { user, users } = useLoaderData<typeof loader>();
+  const fetcher = useFetcher<typeof action>({ key: 'download-docx' });
   const params = useParams();
   const { downloadDocx } = useDownloadDocx();
+  const [isDownload, setIsDownload] = useState(false);
+
+  useEffect(() => {
+    if (fetcher.data && fetcher.data.paragraphs.length && fetcher.data.rollInfo && isDownload) {
+      downloadDocx(fetcher.data.paragraphs, fetcher.data.rollInfo);
+      setIsDownload(false);
+    }
+  }, [fetcher.data, isDownload, downloadDocx]);
+
+  const handleDownload = async () => {
+    fetcher.submit(
+      {
+        rollId: params.rollId || '',
+      },
+      {
+        method: 'post',
+      },
+    );
+    setIsDownload(true);
+  };
+
   return (
     <div className="flex h-full flex-col gap-2 bg-secondary px-4">
       <div className="mt-2 text-xl font-semibold">Tripitaka</div>
       <div className="flex items-center justify-between">
         <BreadcrumbLine />
-        {params.rollId && rollInfo ? (
-          <Button size="icon" variant="ghost" onClick={() => downloadDocx(paragraphs, rollInfo)}>
+        {params.rollId ? (
+          <Button size="icon" variant="ghost" onClick={handleDownload} disabled={fetcher.state !== 'idle'}>
             <Icons.Download className="h-6 w-6 text-slate-800" />
           </Button>
         ) : null}
