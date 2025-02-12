c86b28f7b8bb870143b9df00814d9420df6f0ced
Terry Pan <pttdev123@gmail.com>
2025-02-12 18:34:17 +1000

    fix: fix layout on mobile device

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -15,7 +15,7 @@ export const TranslationCard = ({
         <h3 className="mt-0.5 text-lg font-medium text-gray-900 lg:text-2xl">{title}</h3>
         <h4 className="mt-1 text-sm font-medium text-gray-900">{translator}</h4>
         <div className="mt-4 flex flex-wrap gap-1">
-          <span className="whitespace-nowrap rounded-full bg-purple-100 px-2.5 py-0.5 text-xs text-primary">
+          <span className="whitespace-wrap rounded-full bg-purple-100 px-2.5 py-0.5 text-xs text-primary">
             {subtitle}
           </span>
         </div>
