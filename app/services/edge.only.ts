cce37b334391c392f93d59d465e4bea477d926bc
Terry Pan <pttdev123@gmail.com>
2025-02-16 18:48:51 +1000

    fix: add debug info

--- app/services/edge.only.ts
+++ app/services/edge.only.ts
@@ -25,16 +25,17 @@ export const searchGlossaries = async (searchTerm: string, limit = 5): Promise<R
   });
   if (results.length) {
     if ('hits' in results[0]) {
-      console.log('results', results[0]);
       const ids = results[0].hits.map((hit) => hit.id);
-      console.log('ids', ids);
       const dbResults = await dbClient
         .select()
         .from(glossariesTable)
         .where(inArray(glossariesTable.id, ids))
         .limit(limit);
-      console.log('dbResults', dbResults);
-      return dbResults;
+      // reorder the results based on the ids and filter out undefined values
+      const reorderedResults = ids
+        .map((id) => dbResults.find((result) => result.id === id))
+        .filter((result): result is ReadGlossary => result !== undefined); // Type guard to ensure result is ReadGlossary
+      return reorderedResults;
     }
   }
   return [];
