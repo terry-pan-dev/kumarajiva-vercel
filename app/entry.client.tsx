83ac6cc3c14fe042f4190e7830ffd0decf363a19
Terry Pan <pttdev123@gmail.com>
2025-07-06 22:53:51 +1000

    Trace update (#76)
    * feat: update trace config
    
    * feat: update trace config
    
    * feat: update trace config
    
    * feat: update trace config
    
    * feat: update trace config

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -6,7 +6,7 @@ import { hydrateRoot } from 'react-dom/client';
 process.env.NODE_ENV === 'production' &&
   Sentry.init({
     dsn: 'https://40cbe12a53fcd5340ef69af77c985db3@o4508454077923328.ingest.us.sentry.io/4508454079627264',
-    tracesSampleRate: 0.2,
+    tracesSampleRate: 1.0,
 
     integrations: [
       Sentry.browserTracingIntegration({
--- instrumentation.server.mjs
+++ instrumentation.server.mjs
@@ -3,7 +3,7 @@ import * as Sentry from '@sentry/remix';
 if (process.env.NODE_ENV === 'production') {
   Sentry.init({
     dsn: 'https://40cbe12a53fcd5340ef69af77c985db3@o4508454077923328.ingest.us.sentry.io/4508454079627264',
-    tracesSampleRate: 0.2,
+    tracesSampleRate: 1.0,
     autoInstrumentRemix: true,
   });
   Sentry.replayIntegration({
