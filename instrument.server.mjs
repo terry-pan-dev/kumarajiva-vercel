926324325aea1a53d4be613c4c019dd173724706
Terry Pan <pttdev123@gmail.com>
2025-07-07 20:15:51 +1000

    feat: update sentry config

--- instrument.server.mjs
+++ instrument.server.mjs
@@ -4,7 +4,7 @@ import * as Sentry from '@sentry/remix';
 if (process.env.NODE_ENV === 'production') {
   Sentry.init({
     dsn: 'https://40cbe12a53fcd5340ef69af77c985db3@o4508454077923328.ingest.us.sentry.io/4508454079627264',
-    tracesSampleRate: 1.0,
+    tracesSampleRate: 0.2,
     autoInstrumentRemix: true,
     // Enable trace propagation for your domain
     tracePropagationTargets: ['https://btts-kumarajiva.org'],
