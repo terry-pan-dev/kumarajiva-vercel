5819f0f14b29be1c0db14e9e32da221becc2bd4a
Terry Pan <pttdev123@gmail.com>
2025-07-16 20:42:18 +1000

    Change email provider (#85)
    * feat: send onboarding email for new user
    
    * feat: send onboarding email for new user
    
    * feat: send onboarding email for new user

--- app/routes/_app.admin._index.tsx
+++ app/routes/_app.admin._index.tsx
@@ -4,6 +4,8 @@ import bcrypt from 'bcryptjs';
 import { useMemo } from 'react';
 import { ZodError } from 'zod';
 
+import resend from '~/providers/resend';
+
 import { assertAuthUser } from '../auth.server';
 import { ErrorInfo } from '../components/ErrorInfo';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '../components/ui/tabs';
@@ -80,6 +82,18 @@ export const action = async ({ request }: ActionFunctionArgs) => {
           createdBy: user.id,
           updatedBy: user.id,
         });
+        await resend.emails.send({
+          from: 'Kumarajiva <onboarding@info.btts-kumarajiva.org>',
+          to: newUser.email,
+          subject: 'Welcome to Kumarajiva',
+          html: `<p>Welcome to Kumarajiva! You can login with your email and initial password.</p>
+          <p>Site: <a href="https://btts-kumarajiva.org">https://btts-kumarajiva.org</a></p>
+          <p>Email: ${newUser.email}</p>
+          <p>Password: ${password}</p>
+          <p>Please change your password after login.</p>
+          <p>Thank you for using Kumarajiva!</p>`,
+        });
+        console.log(`email sent to ${newUser.email}`);
       }
     } else if (kind === 'team') {
       const result = validatePayloadOrThrow({ schema: createTeamSchema, formData: data });
