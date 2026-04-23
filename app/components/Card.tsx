2fe13e455944432a5d7ef7a29586436f847906b6
mingness <mingness@users.noreply.github.com>
2026-04-23 19:47:55 +0100

    show origin and target in translation selection (#131)

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -1,23 +1,30 @@
 export const TranslationCard = ({
-  title,
-  subtitle,
-  translator,
+  originTitle,
+  originTranslator,
+  targetTitle,
+  targetTranslator,
   isSelected = false,
 }: {
-  title: string;
-  subtitle: string | null;
-  translator?: string;
+  originTitle: string;
+  originTranslator?: string;
+  targetTitle?: string | null;
+  targetTranslator?: string | null;
   isSelected?: boolean;
 }) => {
   return (
     <article className="hover:animate-background rounded-xl bg-gradient-to-r from-yellow-600 to-slate-700 p-0.5 shadow-xl transition hover:bg-[length:400%_400%] hover:shadow-sm hover:[animation-duration:_4s]">
       <div className={`rounded-[10px] p-4 sm:p-6 lg:!pt-10 ${isSelected ? 'bg-yellow-600' : 'bg-white'}`}>
-        <h3 className="mt-0.5 text-lg font-medium text-gray-900 lg:text-2xl">{title}</h3>
-        <h4 className="mt-1 text-sm font-medium text-gray-900">{translator}</h4>
-        <div className="mt-4 flex flex-wrap gap-1">
-          <span className="whitespace-wrap rounded-full bg-purple-100 px-2.5 py-0.5 text-xs text-primary">
-            {subtitle}
-          </span>
+        <div className={`grid gap-4 ${targetTitle ? 'grid-cols-2' : 'grid-cols-1'}`}>
+          <div>
+            <h3 className="mt-0.5 text-lg font-medium text-gray-900 lg:text-2xl">{originTitle}</h3>
+            {originTranslator && <h4 className="mt-1 text-sm font-medium text-gray-900">{originTranslator}</h4>}
+          </div>
+          {targetTitle && (
+            <div className="border-l border-gray-200 pl-4">
+              <h3 className="mt-0.5 text-lg font-medium text-gray-900 lg:text-2xl">{targetTitle}</h3>
+              {targetTranslator && <h4 className="mt-1 text-sm font-medium text-gray-900">{targetTranslator}</h4>}
+            </div>
+          )}
         </div>
       </div>
     </article>
