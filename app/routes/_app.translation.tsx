5ca60f1f681519f8f76f59c2a5b73f6f55c13c68
Claudine Chen <claudine.chen@gmail.com>
2026-07-17 13:19:13 -0700

    replace sutra and roll services with project and text services

--- app/routes/_app.translation.tsx
+++ app/routes/_app.translation.tsx
@@ -10,11 +10,10 @@ import { SideBarTrigger } from '~/components/SideBarTrigger';
 import { Button } from '~/components/ui/button';
 import { Separator } from '~/components/ui/separator';
 import { Toaster } from '~/components/ui/toaster';
-import { type ReadRollWithSutra } from '~/drizzle/tables';
 import { useDownloadDocx } from '~/lib/hooks';
 import { readUsers } from '~/services';
 import { type IParagraph, readParagraphsByRollId } from '~/services/paragraph.service';
-import { readRollById } from '~/services/roll.service';
+import { getSectionHeading } from '~/services/text.service';
 
 export const meta: MetaFunction = () => {
   return [{ title: 'Translation' }];
@@ -38,19 +37,20 @@ export const action = async ({ request, params }: ActionFunctionArgs) => {
     return redirect('/login');
   }
   const formData = await request.formData();
-  const rollId = formData.get('rollId');
+  const sectionId = formData.get('rollId');
   let paragraphs: IParagraph[] = [];
-  let rollInfo: ReadRollWithSutra | undefined = undefined;
-  if (rollId) {
-    [paragraphs, rollInfo] = await Promise.all([
-      readParagraphsByRollId({ rollId: rollId as string }),
-      readRollById(rollId as string),
+  let sectionInfo: { documentTitle: string; sectionTitle: string | null } | undefined = undefined;
+  if (sectionId) {
+    const [fetchedParagraphs, fetchedSectionInfo] = await Promise.all([
+      readParagraphsByRollId({ rollId: sectionId as string }),
+      getSectionHeading(sectionId as string),
     ]);
+    paragraphs = fetchedParagraphs;
+    sectionInfo = fetchedSectionInfo;
   }
-  console.log('downloading for rollId', rollId);
   return json({
     paragraphs,
-    rollInfo,
+    sectionInfo,
   });
 };
 
@@ -62,8 +62,8 @@ export default function TranslationLayout() {
   const [isDownload, setIsDownload] = useState(false);
 
   useEffect(() => {
-    if (fetcher.data && fetcher.data.paragraphs.length && fetcher.data.rollInfo && isDownload) {
-      downloadDocx(fetcher.data.paragraphs, fetcher.data.rollInfo);
+    if (fetcher.data && fetcher.data.paragraphs.length && fetcher.data.sectionInfo && isDownload) {
+      downloadDocx(fetcher.data.paragraphs, fetcher.data.sectionInfo);
       setIsDownload(false);
     }
   }, [fetcher.data, isDownload, downloadDocx]);
@@ -81,7 +81,7 @@ export default function TranslationLayout() {
   };
 
   return (
-    <div className="flex h-full w-full flex-col bg-secondary px-2 lg:px-4">
+    <div className="bg-secondary flex h-full w-full flex-col px-2 lg:px-4">
       <div className="my-2 flex h-10 w-full items-center justify-between gap-8 text-xl font-semibold">
         <div className="flex h-10 items-center gap-2">
           <SideBarTrigger />
